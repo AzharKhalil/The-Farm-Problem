827ee5b8e52fe8819f4dff1530ad85a91a184f31
AzharKhalil <azharkhalil2018@gmail.com>
2022-07-25 12:12:02 -0700

    Added Solution

---
+++ Animal.java
@@ -0,0 +1,14 @@
+public class Animal{
+    int chickens;
+    int cows;
+    int pigs;
+    
+    public int animals(int chick, int cow, int peg){
+        chickens = chick * 2;
+        cows = cow * 4;
+        pigs = peg * 4;
+        
+        return chickens + cows + pigs;
+    }
+    
+}
---
+++ MyClass.java
@@ -0,0 +1,9 @@
+
+public class MyClass {
+    public static void main(String args[]) {
+        
+        Animal an = new Animal();
+        int total = an.animals(1,2,3);
+        System.out.print(total);
+    }
+}
